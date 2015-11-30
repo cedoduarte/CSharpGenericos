7d712442c9290990ddabc8fda355a5d842a21be6
cedoduarte <carlosduarte.1@hotmail.com>
2015-11-29 20:17:28 -0600

    first commit

---
+++ Genericos/Generico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos
+{
+    public class Generico<Tipo>
+    {
+        public void Imprime()
+        {
+            Console.WriteLine(Valor);
+        }
+
+        public Tipo Valor { get; set; }
+    }
+}
---
+++ Genericos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var valor1 = new Generico<int>();
+            var valor2 = new Generico<string>();
+            var valor3 = new Generico<double>();
+
+            valor1.Valor = 5;
+            valor2.Valor = "Hola, mundo!!!";
+            valor3.Valor = 3.1416;
+
+            valor1.Imprime();
+            valor2.Imprime();
+            valor3.Imprime();
+            Console.Read();
+        }
+    }
+}
